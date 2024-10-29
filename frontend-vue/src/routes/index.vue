c184e0368f1187bc252b6c6e29e4b5d1d0af5d5f
yogi nugroho <Ugroho@proton.me>
2024-10-29 22:37:01 +0700

    finish frontend

--- frontend-vue/src/routes/index.vue
+++ frontend-vue/src/routes/index.vue
@@ -1,6 +1,11 @@
 <script>
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Cookie from 'js-cookie'
+
+// Utility to get the token
+const getToken = () => Cookie.get('token')
+
 const routes = [
     {
         path: '/',
@@ -17,6 +22,29 @@ const routes = [
         name: 'login',
         component: () => import( /* webpackChunkName: "create" */ '../views/auth/login.vue')
     },
+    {
+        path: '/admin/dashboard',
+        name: 'dashboard',
+        component: () => import( /* webpackChunkName: "dashboard" */ '../views/admin/dashboard/index.vue'),
+        meta: { requiresAuth: true }
+    }, {
+        path: '/admin/users',
+        name: 'admin.users.index',
+        component: () => import( /* webpackChunkName: "home" */ '../views/admin/users/index.vue'),
+        meta: { requiresAuth: true } // <-- Add meta field
+    },
+    {
+        path: '/admin/users/create',
+        name: 'admin.users.create',
+        component: () => import( /* webpackChunkName: "home" */ '../views/admin/users/create.vue'),
+        meta: { requiresAuth: true } // <-- Add meta field
+    },
+    {
+        path: '/admin/users/:id',
+        name: 'admin.users.edit',
+        component: () => import( /* webpackChunkName: "home" */ '../views/admin/users/edit.vue'),
+        meta: { requiresAuth: true } // <-- Add meta field
+    }
 ]
 
 //create router
@@ -24,6 +52,26 @@ const router = createRouter({
     history: createWebHistory(),
     routes //
 })
+
+router.beforeEach((to, from, next) => {
+    // Ambil token otentikasi pengguna
+    const token = getToken();
+
+    // Jika rute tujuan membutuhkan otentikasi dan pengguna tidak memiliki token
+    if (to.matched.some(record => record.meta.requiresAuth) && !token) {
+        // Alihkan pengguna ke halaman login
+        next({ name: 'login' });
+    }
+    // Jika rute tujuan adalah halaman login atau register dan pengguna sudah login
+    else if ((to.name === 'login' || to.name === 'register') && token) {
+        // Alihkan pengguna ke halaman dashboard
+        next({ name: 'dashboard' });
+    }
+    // Jika tidak ada kondisi khusus, izinkan navigasi ke rute tujuan
+    else {
+        next();
+    }
+});
 
 export default router
 </script>
